92b45e82f19eaeb928df84f6a8c461a0483d3385
Robertus Dwi Ari Utomo <robertus.ari.utomo@gmail.com>
2020-07-14 21:08:18 +0700

    New Update

---
+++ Assets/PergerakanPlatform.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PergerakanPlatform : MonoBehaviour
+{
+    public float amplitudo;
+    public float kecepatan;
+
+    void Update()
+    {
+        transform.position = new Vector3(amplitudo * Mathf.Sin((Time.time)*kecepatan), this.transform.position.y, 0f);
+    }
+}
