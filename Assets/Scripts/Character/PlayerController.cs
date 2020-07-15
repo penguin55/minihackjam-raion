3377d029da90fbd251f139896224812c692676d6
Robertus Dwi Ari Utomo <robertus.ari.utomo@gmail.com>
2020-07-15 22:32:50 +0700

    Update Game Management and Ui Pause

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -48,7 +48,7 @@ public class PlayerController : PlayerBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.tag.Equals("Finish"))
+        if (collision.collider.tag.Equals("Moving"))
         {
             this.transform.parent = collision.collider.transform;
         }
@@ -56,7 +56,7 @@ public class PlayerController : PlayerBehaviour
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.collider.tag.Equals("Finish"))
+        if (collision.collider.tag.Equals("Moving"))
         {
             this.transform.parent = null;
         }
---
+++ Assets/Scripts/GameManagement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagement : MonoBehaviour
+{
+    [SerializeField] private GameObject panelPause;
+    private bool gameFreeze;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameFreeze = !gameFreeze;
+            Pause(gameFreeze);
+        }
+    }
+
+    public void Pause(bool flag)
+    {
+        Time.timeScale = flag ? 0 : 1; 
+        panelPause.SetActive(flag);
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Quit()
+    {
+
+    }
+
+}
