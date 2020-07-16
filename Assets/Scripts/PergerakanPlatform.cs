762cdfc7abe5959990b6c91a2e657d7864ea8636
Robertus Dwi Ari Utomo <robertus.ari.utomo@gmail.com>
2020-07-17 02:00:21 +0700

    Update moving platform

--- Assets/Scripts/PergerakanPlatform.cs
+++ Assets/Scripts/PergerakanPlatform.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class PergerakanPlatform : MonoBehaviour
 {
+    [Tooltip("Pakai x = 1 buat gerak horizontal, y = 1 buat gerak vertikal. Keduanya buat diagonal")] 
+    public Vector2 direction;
+
+    [Tooltip("Biar jauh dan lebar pergerakan moving platformnya, geraknya pakai acuan titik tengah dari platform ini")]
     public float amplitudo;
     public float kecepatan;
 
+    private Vector3 basePosition;
+
+    private void Start()
+    {
+        basePosition = transform.position;
+    }
+
     void Update()
     {
-        transform.position = new Vector3(amplitudo * Mathf.Sin((Time.time)*kecepatan), this.transform.position.y, 0f);
+        transform.position = basePosition + (Vector3) (direction * amplitudo * Mathf.Sin((Time.time) * kecepatan));
     }
 }
