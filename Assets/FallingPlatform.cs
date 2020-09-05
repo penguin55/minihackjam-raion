444872182ea9ef4babcc1a50b946057655505f16
Muhammad Rafif Al Aziz <mrafifalaziz26@gmail.com>
2020-09-05 13:24:05 +0700

    Add Falling Platform

---
+++ Assets/FallingPlatform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : FallingPlatformTriggerer
+{
+
+    public static bool fall;
+    public float timeToFall;
+
+    private void Update()
+    {
+        if (fall)
+        {
+            letsFall();
+            fall = false;
+        }
+    }
+
+    private void letsFall()
+    {
+        StartCoroutine(falling());
+        Debug.Log("enter");
+    }
+
+    IEnumerator falling()
+    {
+        yield return new WaitForSeconds(timeToFall);
+        FallingPlatform parentGO = gameObject.GetComponentInParent<FallingPlatform>();
+        parentGO.gameObject.SetActive(false);
+        Debug.Log("hancur");
+    }
+}
---
+++ Assets/FallingPlatformTriggerer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatformTriggerer : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        FallingPlatform.fall = true;
+    }
+
+}
