6569e92a29a880dee493f081c423d5295d242313
LuckyPotatoe <callmedanny404@gmail.com>
2020-09-11 20:51:21 +0700

    Added parallax and infinite scrolling background

---
+++ Assets/Scripts/BackgroundLoop.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundLoop : MonoBehaviour
+{
+    public GameObject[] background;
+    private Camera mainCamera;
+    private Vector2 screenBounds;
+    
+    void Start()
+    {
+        mainCamera = gameObject.GetComponent<Camera>();
+        screenBounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z));
+        
+        foreach(GameObject obj in background)
+        {
+            loadChildObject(obj);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        foreach (GameObject obj in background)
+        {
+            childReposition(obj);
+        }
+    }
+
+    void loadChildObject(GameObject obj)
+    {
+        float objectWidth = obj.GetComponent<SpriteRenderer>().bounds.size.x;
+        int childNeeded = (int)Mathf.Ceil(screenBounds.x * 2/ objectWidth);
+        GameObject clone = Instantiate(obj) as GameObject;
+
+        for (int i = 0; i <= childNeeded; i++)
+        {
+            GameObject c = Instantiate(clone) as GameObject;
+            c.transform.SetParent(obj.transform);
+            c.transform.position = new Vector3(objectWidth * i, obj.transform.position.y, obj.transform.position.z);
+            c.name = obj.name + i;
+        }
+
+        Destroy(clone);
+        Destroy(obj.GetComponent<SpriteRenderer>());
+    }
+
+    void childReposition(GameObject obj)
+    {
+        Transform[] children = obj.GetComponentsInChildren<Transform>();
+
+        if(children.Length > 1)
+        {
+            GameObject firstChild = children[1].gameObject;
+            GameObject lastChild = children[children.Length-1].gameObject;
+
+            float halfObjectWidth = lastChild.GetComponent<SpriteRenderer>().bounds.extents.x;
+
+            if(transform.position.x + screenBounds.x > lastChild.transform.position.x + halfObjectWidth)
+            {
+                firstChild.transform.SetAsLastSibling();
+                firstChild.transform.position = new Vector3(lastChild.transform.position.x + halfObjectWidth * 2, lastChild.transform.position.y, lastChild.transform.position.z);
+            }
+            else if(transform.position.x - screenBounds.x < firstChild.transform.position.x - halfObjectWidth)
+            {
+                lastChild.transform.SetAsFirstSibling();
+                lastChild.transform.position = new Vector3(firstChild.transform.position.x - halfObjectWidth * 2, firstChild.transform.position.y, firstChild.transform.position.z);
+            }
+        }
+    }
+}
